f9d2a4c470ad13a812be22f7daf1bbaf8d4d94cd
Loving Programming <officialstanlee143@gmail.com>
2026-02-09 13:16:22 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,18 @@
 module.exports = {
     botName: "ɪɴꜱɪᴅɪᴏᴜꜱ: ᴛʜᴇ ʟᴀꜱᴛ ᴋᴇʏ",
     ownerName: "ꜱᴛᴀɴʏᴛᴢ",
-    ownerNumber: "255618558502", // Namba yako bila +
+    ownerNumber: "255xxxxxxxxx", // Without +
     prefix: ".", 
-    workMode: "public", // 'public' au 'private'
-    mongodb: "YOUR_MONGODB_URL", // Muhimu kwa Always Online
+    mongodb: "YOUR_MONGODB_URL", // Required for Always Online
     newsletterJid: "120363404317544295@newsletter",
+    groupJid: "120363406549688641@g.us",
     footer: "© 2025 ꜱᴛᴀɴʏᴛᴢ ᴛᴇᴄʜ",
+    
+    // Feature Toggles
+    autoStatus: { view: true, like: true, reply: true },
+    autoRead: true,
+    autoSave: true,
+    antiCall: true,
+    workMode: "public", // 'public' or 'private'
     sessionName: "session"
 };
