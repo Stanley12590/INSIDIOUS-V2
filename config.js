ed2294cc524dc55780b642dce8f57e9c62d76ffc
Loving Programming <officialstanlee143@gmail.com>
2026-02-09 12:55:24 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    botName: "ɪɴꜱɪᴅɪᴏᴜꜱ: ᴛʜᴇ ʟᴀꜱᴛ ᴋᴇʏ",
+    ownerName: "ꜱᴛᴀɴʏᴛᴢ",
+    ownerNumber: "255618558502", // Namba yako bila +
+    prefix: ".", 
+    workMode: "public", // 'public' au 'private'
+    mongodb: "YOUR_MONGODB_URL", // Muhimu kwa Always Online
+    newsletterJid: "120363404317544295@newsletter",
+    footer: "© 2025 ꜱᴛᴀɴʏᴛᴢ ᴛᴇᴄʜ",
+    sessionName: "session"
+};
