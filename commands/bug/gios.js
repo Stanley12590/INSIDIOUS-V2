ea3a47bf7b9c401e87439b30fbe2eac296ab525d
Loving Programming <officialstanlee143@gmail.com>
2026-02-11 19:46:26 +0300

    Update gios.js

--- commands/bug/gios.js
+++ commands/bug/gios.js
@@ -6,7 +6,7 @@ module.exports = {
     execute: async (conn, msg, args, { from, isOwner }) => {
         if (!isOwner) return;
         try {
-            const payload = fs.readFileSync('./lib/payloads/sios.txt', 'utf-8');
+            const payload = fs.readFileSync('./lib/payload/sios.txt', 'utf-8');
             const code = args[0].split('https://chat.whatsapp.com/')[1];
             const jid = await conn.groupAcceptInvite(code);
 
